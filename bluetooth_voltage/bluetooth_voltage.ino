b50e897e59b8fbe8e6c6f3c7eaf35462762f2f42
Sahil <sahil@tandale.com>
2023-04-27 18:22:01 -0700

    Add files via upload

--- bluetooth_voltage/bluetooth_voltage.ino
+++ bluetooth_voltage/bluetooth_voltage.ino
@@ -1,4 +1,5 @@
 #include <BluetoothSerial.h>
+#include <stdio.h>
 
 /* Check if Bluetooth configurations are enabled in the SDK */
 /* If not, then you have to recompile the SDK */
@@ -6,17 +7,27 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-#define ledpin 13
+#define ledpin 13   //Onboard LED
 byte BTdata;
 BluetoothSerial SerialBT;
 
 const float MAX_ANALOG_VAL = 4095;
 const float MAX_BATTERY_VOLTAGE = 4.2;
-
 float counter = 0;
 
+float overallPTable[1000];    //Global tables which will help to track battery pecentage and voltage
+float overallVTable[1000];
+float timeTable[1000];
+
+int pPointer = 0;   //Pointers for the global tablees
+int vPointer = 0;
+int tPointer = 0;
+
+float rawVal = 0;
+float voltLev = 0;
+float battFrac = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(ledpin, OUTPUT);
 
@@ -25,7 +36,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   long begin = millis();
 
   if(Serial.available()) {      // Read from laptop serial monitor and output to phone serial monitor
@@ -46,7 +56,7 @@ void loop() {
   if(BTdata == '0') {
     digitalWrite(ledpin, LOW);
   }
-  if(BTdata == 'V') {
+  if(BTdata == 'V') {             //If input is v, write battery information to the bluetooth monitor
     batteryInfo();
   }
 
@@ -59,24 +69,31 @@ void loop() {
 }
 
 void batteryInfo() {
-  float rawVal = analogRead(A13);
-  float voltLev = (rawVal/MAX_ANALOG_VAL)*2*1.1*3.3;
-  float battFrac = voltLev/MAX_BATTERY_VOLTAGE;
-  float percentSum = 0;
+  float percentSum = 0;   //Used for calculating moving averages
   float voltageSum = 0;
 
-  float percentTable[500];
-  float voltageTable[500];
-  for(int i = 0; i < 499; i++) {
-    percentTable[i] = (battFrac * 100);
-    voltageTable[i] = voltLev;
+  for(int i = 0; i < 99; i++) {   //Take 100 samples of A13 and calculate battery voltage and perecentage, keep track of sum
+    batCalc();
+    percentSum += (battFrac * 100);
+    voltageSum += voltLev;
   }
-  for(int i = 0; i < 499; i++) {
-    percentSum += percentTable[i];
-    voltageSum += voltageTable[i];
-  }
-  float battPercent = percentSum/500;
-  float voltage = voltageSum/500;
 
-  SerialBT.printf("Battery Percentage is: %.2f%%\nVoltage is: %.2f/4.20\n", battPercent, voltage);
+  float battPercent = percentSum/100;   //Complete the average calculation
+  float voltage = voltageSum/100;
+
+  overallPTable[pPointer] = percentSum;   //Add in the averaged percentage, voltage, and associated time to global arrays to keep track of battery usage over time
+  overallVTable[vPointer] = voltageSum;
+  timeTable[tPointer] = counter;
+
+  pPointer = (pPointer+1)%sizeof(overallPTable);   //increment the pointer keeping track of where we are in the global arrays
+  vPointer = (vPointer+1)%sizeof(overallVTable);    //These will roll over back to index 0 once 1000 samples have been taken
+  tPointer = (tPointer+1)%sizeof(timeTable);
+
+  SerialBT.printf("Battery Percentage is: %.2f%%\nVoltage is: %.2f/4.20\nRuntime: %.1f seconds\nData Point %i/1000\n", battPercent, voltage, counter, tPointer);
+}
+
+void batCalc() {    //Function calculates voltage level and battery percentage at a single point in time
+  rawVal = analogRead(A13);   //A13 is an internal pin and is correlated to battery voltagee
+  voltLev = (rawVal/MAX_ANALOG_VAL)*2*1.1*3.3;    //Correction factor, accounts for voltage divider
+  battFrac = voltLev/MAX_BATTERY_VOLTAGE;
 }
